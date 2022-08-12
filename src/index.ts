defb420c39af6d5035b9631f21dc7a225990668e
Nikolay Dorofeev <dorich2000@gmail.com>
2022-08-12 19:06:06 +0400

    render nodes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import { build } from './buildGraph'
 import { parse } from './parseInfo'
+import { IDepNode } from './types/dependenciesGraph'
 
 async function main(){
-  const { repos, depNodes, 
-    dockerUsageMap, reposDepandencies, reposTypesMap } = await parse()
-  
+  const { repos, depNodes, reposDepandencies,
+    dockerUsageMap, reposTypesMap } = await parse()
+  const allNodes: IDepNode[] = [...depNodes]
+  for (const [repoName, dependencies] of reposDepandencies) {
+    for(const dep of dependencies) {
+      if (!allNodes.includes(dep)) allNodes.push(dep)
+    }
+  }
+  build(allNodes, [])
 }
 
 main()
