bfc87abe2cedd2bc50e5dcd5ee5df9b616fa9d3e
Nikolay Dorofeev <dorich2000@gmail.com>
2022-08-19 12:08:50 +0400

    Create `output` folder if not exist

--- src/render.ts
+++ src/render.ts
@@ -3,6 +3,9 @@ const nplantuml = require('node-plantuml');
 import fs from 'fs'
 
 export async function render(plantUml: string){
+  const folders = fs.readdirSync('./')
+  if (!folders.includes('output')) fs.mkdirSync('output')
+  
   fs.writeFileSync('output/schema.puml', plantUml)
   const svg = await plantuml(plantUml)
 
